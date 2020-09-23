eb77b91005d95283bcdc9e6cfdf92f2e9bfb23e3
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-09-23 09:11:28 +0700

    pet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontSizeComponent } from './font-size/font-size.component';
+import {FormsModule} from '@angular/forms';
+import { PetComponent } from './pet/pet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FontSizeComponent,
+    PetComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
